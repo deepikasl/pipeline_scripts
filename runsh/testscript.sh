a7f004981b2296de430fd7018cfbc521eb3d3661
shiptest-rc-ow <shiptest.rc.ow@gmail.com>
2017-02-23 18:24:28 +0530

    Update testscript.sh

--- runsh/testscript.sh
+++ runsh/testscript.sh
@@ -12,8 +12,8 @@ export TEST_RES_REPO_UP=$(echo $TEST_RES_REPO | awk '{print toupper($0)}')
 export TEST_RES_REPO_STATE=$(eval echo "$"$TEST_RES_REPO_UP"_STATE")
 
 export TEST_CURR_JOB_UP=$(echo $TEST_CURR_JOB | awk '{print toupper($0)}')
-echo $TEST_CURR_JOB=TEST_CURR_JOB
-echo $TEST_CURR_JOB_UP=TEST_CURR_JOB_UP
+echo TEST_CURR_JOB=$JOB_NAME
+echo TEST_CURR_JOB_UP=TEST_CURR_JOB_UP
 
 #get dockerhub EN string
 export TEST_RES_DH_UP=$(echo $TEST_RES_DH | awk '{print toupper($0)}')
