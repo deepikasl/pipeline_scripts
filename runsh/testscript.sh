d76a24f786df7fc30e29477cb6022eec7096ba19
shiptest-rc-ow <shiptest.rc.ow@gmail.com>
2017-05-07 16:25:53 +0530

    Update testscript.sh

--- runsh/testscript.sh
+++ runsh/testscript.sh
@@ -159,12 +159,12 @@ get_params() {
 }
 
 create_out_state() {
-  echo "-----> Creating a state file for $TEST_RES_IMG_OUT_UP"
-  echo versionName=$TEST_VERSION > "$JOB_STATE/$TEST_RES_IMAGE_OUT.env"
+  echo '-----> Creating a state file for $TEST_RES_IMG_OUT_UP'
+  echo versionName=$TEST_REPO_VERSION > "$JOB_STATE/$TEST_RES_IMAGE_OUT.env"
   echo commitSHA=$TEST_REPO_COMMIT >> "$JOB_STATE/$TEST_RES_IMAGE_OUT.env" 
   
   echo "-----> Creating a state file for $TEST_CURR_JOB"
-  echo versionName=$TEST_VERSION > "$JOB_STATE/$TEST_CURR_JOB.env"
+  echo versionName=$TEST_REPO_VERSION > "$JOB_STATE/$TEST_CURR_JOB.env"
   cat "$JOB_STATE/$TEST_CURR_JOB.env"
   
   echo "-----> Creating a previous state file for $TEST_CURR_JOB"
